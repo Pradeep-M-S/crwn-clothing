691f366025485ac1aaef326f3ac8b2603096d30a
Pradeep-M-S <pradeepms200076@gmail.com>
2020-10-29 15:09:20 +0530

    adding header and link to routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/ShopPage/ShopPage";
+import Header from "./components/Header/Header";
 
 function HatsPage(props) {
   console.log(props);
@@ -15,6 +16,7 @@ function HatsPage(props) {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />{" "}
         <Route exact path="/shop" component={ShopPage} />
